873753f5ff0c1db2e56aa1159d9078cf1033f0a3
Sebastian Ullrich <sebasti@nullri.ch>
2024-08-02 13:17:56 +0200

    more missing includes

--- src/library/compiler/init_module.h
+++ src/library/compiler/init_module.h
@@ -5,6 +5,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #pragma once
+#include "runtime/object.h"
+
 namespace lean {
 LEAN_EXPORT void initialize_compiler_module();
 LEAN_EXPORT void finalize_compiler_module();
